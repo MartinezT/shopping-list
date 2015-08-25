c2de796cd66fee60dc84e162334484c35879d8f5
MartinezT <martinez.t@live.com>
2015-08-24 22:14:14 -0600

    add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
-	$(this).on('click','.item', function(){
-		$(this).remove();
-	});
-    $('button').click(function() {
-    	var toAdd = $("input[name=shopping-list]").val();
-        $('#list').append("<div class ='item'>"+toAdd+"</div>");
+$(document).ready(function () {
+    $(this).on('click', '.item', function () {
+        $(this).remove();
+    });
+    $('button').click(function () {
+        var toAdd = $("input[name=shopping-list]").val();
+        $('#list').append("<div class ='item'>" + toAdd + "</div>");
         $("form").trigger("reset");
-
     });
 });
