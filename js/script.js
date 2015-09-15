c8dc0c0cc00dde1aaee7c6b38c2bf3961f6bbb00
MartinezT <martinez.t@live.com>
2015-09-15 15:14:52 -0600

    added js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+
 $(document).ready(function () {
     $('#button1').click(function () {
         var toAdd = $("input[name=shopping-list]").val();
-        $('#list').append("<div class ='item'><input type='checkbox' name='checkItem' id='checkItem'>"+toAdd+"</div>");
+        $('#listItem').append("<li class ='item'><input type='checkbox' name='checkItem' id='checkItem'>"+toAdd+"</li>");
         $("form").trigger("reset");
     });
-    $('#button2').click(function(){
-    	if($('[type="checkbox"]').is(":checked")){
-    		$('.item').remove();
-    	}
+    $('#button2').on('click',function(){
+        $("input:checkbox").each(function(){
+            if ($(this).is(":checked")){
+                $(this).parent().remove();
+            };
+        });
     });
 });
