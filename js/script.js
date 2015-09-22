5ecab96f1a04fd37c3c5c2ba7bf13cc6c32c8d5f
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2015-09-22 19:25:08 -0400

    Use submit handler

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
-    $('#button1').click(function () {
-        if($("input[name=shopping-list]").val() == ''){
-            alert('Input can not be left blank');
-            return false;
-        } else
-            var toAdd = $("input[name=shopping-list]").val();
-            $('#listItem').append("<li class ='item'><input type='checkbox' name='checkItem' id='checkItem'>"+toAdd+"</li>");
-            $("form").trigger("reset");
-    });
-
     $('#button2').on('click',function(){
         $("input:checkbox").each(function(){
             if ($(this).is(":checked")){
@@ -18,17 +8,12 @@ $(document).ready(function () {
     });
      $("form").submit(function(e){
         e.preventDefault();
+        if($("input[name=shopping-list]").val() == ''){
+            alert('Input can not be left blank');
+            return false;
+        }
+        var toAdd = $("input[name=shopping-list]").val();
+        $('#listItem').append("<li class ='item'><input type='checkbox' name='checkItem' id='checkItem'>"+toAdd+"</li>");
+        $("form").trigger("reset");
     });
-    $("input").keydown(function(e){
-        if(e.keyCode==13) {
-            if($("input[name=shopping-list]").val() == ''){
-                alert('Input can not be left blank');
-                  return false;
-        } else
-            var toAdd = $("input[name=shopping-list]").val();
-            $('#listItem').append("<li class ='item'><input type='checkbox' name='checkItem' id='checkItem'>"+toAdd+"</li>");
-            $("form").trigger("reset");
-            e.preventDefault();
-        } 
-    })
 });
